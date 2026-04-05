15fc12775b36a1d7d6cc82037d97a9d6023619e7
Kidus Yosef <148970622+kid-yP@users.noreply.github.com>
2026-04-05 07:08:00 +0300

    Update authRoute.js

--- Backend/routes/authRoute.js
+++ Backend/routes/authRoute.js
@@ -1,17 +1,26 @@
 import express from "express";
-import { 
-    login, 
-    signup, 
-    logOut, 
-    getMe,
-    AccessRefreshToken, 
-    
+import {
+  login,
+  signup,
+  logOut,
+  getMe,
+  AccessRefreshToken,
+  verifyEmail,
+  forgotPassword,
+  resetPassword
 } from "../controllers/authController.js";
 import { verifyToken } from "../middleware/authentication.js";
+
 const authRoutes = express.Router();
-authRoutes.post("/login",login)
-authRoutes.post("/register",signup)
-authRoutes.post("/logout",verifyToken,logOut)
-authRoutes.post("/refresh",AccessRefreshToken)
-authRoutes.get("/me",verifyToken,getMe)
+
+authRoutes.post("/login", login);
+authRoutes.post("/register", signup);
+authRoutes.post("/logout", verifyToken, logOut);
+authRoutes.post("/refresh", AccessRefreshToken);
+authRoutes.get("/me", verifyToken, getMe);
+
+authRoutes.get("/verify-email/:token", verifyEmail);
+authRoutes.post("/forgot-password", forgotPassword);
+authRoutes.post("/reset-password/:token", resetPassword);
+
 export default authRoutes;
