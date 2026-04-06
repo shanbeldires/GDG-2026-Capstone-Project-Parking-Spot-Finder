142e6415f08e6faa419bf883094578fd3d335b5c
Shanbel Dires <shanbeldires7@gmail.com>
2026-04-06 14:20:01 +0300

    comment cors part

--- Backend/app.js
+++ Backend/app.js
@@ -1,19 +1,19 @@
 import express from "express";
-import cors from "cors";
+// import cors from "cors";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import morgan from "morgan";
 import errorHandler from "./errorHandler/errorHandler.js";
 import dotenv from "dotenv";
 dotenv.config();
-import { CLIENT_URL } from "./config/env.js";
+// import { CLIENT_URL } from "./config/env.js";
 import authRoutes from "./routes/authRoute.js";
 import parkingRoutes from "./routes/parkingRoute.js"
 import reservationRoutes from "./routes/reservationRoute.js";
 const app = express();
 
 // Middlewares
-app.use(cors({ origin:CLIENT_URL, credentials: true }));
+// app.use(cors({ origin:CLIENT_URL, credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 app.use(morgan("dev"));
